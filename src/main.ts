c929cacfdccc4ac2f49b0d28f625b6fb2c35f64b
Serj <serj.shorokhov.dev@gmail.com>
2025-02-27 18:31:06 +0300

    Add validation for `post` method

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: process.env.NODE_ENV == 'production',
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,5 +1,21 @@
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
 export class CreateTodoDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(255)
   name: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsBoolean()
   status: boolean;
 }

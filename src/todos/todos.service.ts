dd049c24fe8379dab328d4b3b917f724ea775e94
Serj <serj.shorokhov.dev@gmail.com>
2025-02-28 13:32:44 +0300

    refactor: simplify async methods in TodosService

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -6,23 +6,23 @@ import { CreateTodoData, UpdateTodoData } from './types/todo.types';
 export class TodosService {
   constructor(private readonly repository: TodoRepository) {}
 
-  async create(todo: CreateTodoData) {
-    return await this.repository.create(todo);
+  create(todo: CreateTodoData) {
+    return this.repository.create(todo);
   }
 
-  async findAll() {
-    return await this.repository.findAll();
+  findAll() {
+    return this.repository.findAll();
   }
 
-  async findOne(id: string) {
-    return await this.repository.findById(id);
+  findOne(id: string) {
+    return this.repository.findById(id);
   }
 
-  async update(id: string, updateTodoDto: UpdateTodoData) {
-    return await this.repository.update(id, updateTodoDto);
+  update(id: string, updateTodoDto: UpdateTodoData) {
+    return this.repository.update(id, updateTodoDto);
   }
 
-  async remove(id: string) {
-    return await this.repository.delete(id);
+  remove(id: string) {
+    return this.repository.delete(id);
   }
 }
